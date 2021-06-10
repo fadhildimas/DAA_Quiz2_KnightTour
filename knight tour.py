423031b964e88caeaa3e833ac3c94f3db1417380
naufalfabian <naufalfabian321@gmail.com>
2021-06-10 09:47:45 +0700

    add class node

--- knight tour.py
+++ knight tour.py
@@ -37,3 +37,42 @@ class Problem:
             return True
         else:
             return False
+
+
+class Node:
+
+
+        # location of node on board
+        self.location = None    
+
+        # problem that the node belongs to
+        self.problem = problem
+
+        # parent of the node
+        self.parent = parent
+
+        # action that is taken to reach this node
+        self.action = action
+
+        # if node doesn't have a parent, then it is the root node so its state is initialized with the initial state
+        # location is set to initial state as well
+        if self.parent is None:
+            self.state = []
+            self.state.append(self.problem.initialState)
+            self.location = self.problem.initialState
+        
+        # if it is not the root node enters here to set its state and location
+        # gets the state of its parent and then current location of the node is appended to state
+        else:
+            self.state = self.parent.state[:]
+            new_x = self.parent.location[0] + self.action[0]
+            new_y = self.parent.location[1] + self.action[1]
+            self.state.append([new_x, new_y])
+            self.location = [new_x, new_y]
+
+# checks if a child node is already in the frontier or not
+def isChildInFrontier(frontier, child):
+    for node in frontier:
+        if node.state == child.state:
+            return True
+    return False
